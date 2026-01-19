41cb5fec62cd9a44acfd2ad26b999228c97b0964
Andy Fernandez <afgarabote@gmail.com>
2026-01-19 11:45:03 -0300

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { NextIntlClientProvider } from 'next-intl';
 import { getLocale, getMessages, getTranslations } from 'next-intl/server';
+import { Analytics } from "@vercel/analytics/next"
 import { Providers } from "./providers";
 import { Header } from "@/components";
 import "./globals.css";
@@ -44,6 +45,7 @@ export default async function RootLayout({
               <Header />
               <main className="flex-1">
                 {children}
+                <Analytics />
               </main>
             </div>
           </Providers>
